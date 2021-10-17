2cdf1e79951603e9099cfb09fea02320b4db3ad0
JJJerome <JosephJerome94@gmail.com>
2021-10-17 12:32:04 +0100

    Add script for getting a Pandas LOB dataframe (for data analysis)

---
+++ RL4MM/extras/get_book_trade_dataframe.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from RL4MM.database.populate_database import get_lobster_data, reformat_message_data, rescale_book_data
+
+
+def get_book_trade_dataframe(
+    ticker: str = "AMZN",
+    trading_date: str = "2012-06-21",
+    levels: int = 10,
+):
+    print(f"Getting data for ticker {ticker} on {trading_date}.")
+    books, messages = get_lobster_data(ticker, trading_date, levels)
+    messages = reformat_message_data(messages, trading_date)
+    books = rescale_book_data(books)
+    book_trade_df = pd.concat([messages, books], axis=1)
+    return book_trade_df.set_index("timestamp")
