1802743bacba1b40fe1b766b725ee0fc5b1adee5
JJJerome <JosephJerome94@gmail.com>
2021-10-29 17:46:38 +0100

    Add Backtester and BacktestAnalyser

---
+++ RL4MM/backtest/Backtester.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+from tqdm import tqdm
+
+import pandas as pd
+
+from RL4MM.agents.Agent import Agent
+from RL4MM.simulator.OrderbookSimulator import OrderbookSimulator, HistoricalOrderbookSimulator
+
+
+class Backtester:
+    def __init__(
+        self, agent: Agent, simulator: OrderbookSimulator = None, initial_portfolio: dict = {"cash": 0, "stock": 0}
+    ) -> None:
+        self.agent = agent
+        self.simulator = simulator or self._get_default_simulator()
+        self.initial_portfolio = initial_portfolio
+        self.results = dict()
+
+    def run(self, start_date: datetime, end_date: datetime, step_size: timedelta, start_book: pd.Series) -> None:
+        messages_to_fill = list()
+        filled_messages = list()
+        book = start_book
+        for timestamp in tqdm(self._get_daterange(start_date, end_date, step_size)):
+            new_messages = self.agent.generate_messages(timestamp, book)
+            messages_to_fill += new_messages
+            messages_to_fill, new_filled, book = self.simulator.simulate_step(
+                timestamp, timestamp + step_size, messages_to_fill, book
+            )
+            filled_messages += new_filled
+        results = {
+            "start date": start_date,
+            "end date": end_date,
+            "step size": step_size,
+            "start book": start_book,
+            "remaining messages": messages_to_fill,
+            "filled messages": filled_messages,
+            "terminal book": book,
+        }
+        self.results = results
+
+    @staticmethod
+    def _get_default_simulator():
+        return HistoricalOrderbookSimulator(exchange="NASDAQ", ticker="MSFT")
+
+    @staticmethod
+    def _get_daterange(start_date: datetime, end_date: datetime, step_size: timedelta):
+        for n in range(int((end_date - start_date) / step_size)):
+            yield start_date + n * step_size
--- RL4MM/extras/get_book_message_dataframe.py
+++ RL4MM/extras/get_book_message_dataframe.py
@@ -3,7 +3,7 @@ import pandas as pd
 from RL4MM.database.populate_database import get_lobster_data, reformat_message_data, rescale_book_data
 
 
-def get_book_trade_dataframe(
+def get_book_message_dataframe(
     ticker: str = "AMZN",
     trading_date: str = "2012-06-21",
     levels: int = 10,
@@ -12,6 +12,6 @@ def get_book_trade_dataframe(
     books, messages = get_lobster_data(ticker, trading_date, levels)
     messages = reformat_message_data(messages, trading_date)
     books = rescale_book_data(books)
-    book_trade_df = pd.concat([messages, books], axis=1)
-    book_trade_df.columns = book_trade_df.columns.str.replace('_',' ')
-    return book_trade_df.set_index("timestamp")
+    book_message_df = pd.concat([messages, books], axis=1)
+    book_message_df.columns = book_message_df.columns.str.replace("_", " ")
+    return book_message_df.set_index("timestamp")
