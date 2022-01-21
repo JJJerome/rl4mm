923192b81114343ba740ece8956f07a777c1ddfc
JJJerome <JosephJerome94@gmail.com>
2022-01-21 16:41:31 +0000

    Add some simple baseline agents (one specifically for AS env)

---
+++ RL4MM/agents/BaselineAgents.py
@@ -0,0 +1,21 @@
+import gym
+
+from RL4MM.agents.Agent import Agent
+from RL4MM.base import State, Action
+
+
+class RandomAgent(Agent):
+    def __init__(self, env: gym.Env):
+        self.env = env
+
+    def get_action(self, state: State) -> Action:
+        return self.env.action_space.sample()
+
+
+class FixedSpreadAgent(Agent):
+    def __init__(self, half_spread: float = 1.0, offset: float = 0.0):
+        self.half_spread = half_spread
+        self.offset = offset
+
+    def get_action(self, state: State):
+        return np.array([self.half_spread - self.offset, self.half_spread + self.offset])
