d2bf11a84e6be3ec9ccbd55bcea4dd4198ec8f13
JJJerome <JosephJerome94@gmail.com>
2022-02-15 17:14:14 +0000

    Temporarily exclude historical simulation and feature generation from mypy checks

--- tasks.py
+++ tasks.py
@@ -5,6 +5,10 @@ MODULES_TO_CHECK = ["RL4MM", "*.py"]
 MODULES_TO_CHECK_STR = " ".join(MODULES_TO_CHECK)
 BLACK_PATHS_TO_IGNORE = []
 BLACK_PATHS_TO_IGNORE_STR = " ".join(BLACK_PATHS_TO_IGNORE)
+MYPY_PATHS_TO_IGNORE = ["RL4MM/simulator/", "RL4MM/features/"]
+MYPY_EXCLUSION_STR = ""
+for path in MYPY_PATHS_TO_IGNORE:
+    MYPY_EXCLUSION_STR += " --exclude " + path
 
 
 @task
@@ -21,7 +25,11 @@ def check_python(c):
         c.run(f"black --check --line-length 120 {MODULES_TO_CHECK_STR} --exclude {BLACK_PATHS_TO_IGNORE_STR}")
     else:
         c.run(f"black --check --line-length 120 {MODULES_TO_CHECK_STR}")
+    print("Running flake8...")
     c.run(f"flake8 {MODULES_TO_CHECK_STR}")
-    c.run(f"mypy {MODULES_TO_CHECK_STR}")
+    print("No flake8 errors")
+    print("Running mypy...")
+    c.run(f"mypy -p {MODULES_TO_CHECK[0]}" + MYPY_EXCLUSION_STR)
+    print("No mypy errors")
     c.run("python check_init_files.py")
     c.run("python check_all_py_imports.py")
