700ef77cdcd182cd84f49910b9282e4496f8cc00
JJJerome <JosephJerome94@gmail.com>
2022-03-01 11:49:07 +0000

    Add capacity to get_expected_action to Agents

--- RL4MM/agents/Agent.py
+++ RL4MM/agents/Agent.py
@@ -4,5 +4,9 @@ import numpy as np
 
 
 class Agent(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     def get_action(self, state: np.ndarray) -> np.ndarray:
         pass
+
+    def get_expected_action(self, state: np.ndarray, n_samples: int = 1000) -> np.ndarray:
+        return np.array([self.get_action(state) for _ in range(n_samples)]).mean(axis=0)
