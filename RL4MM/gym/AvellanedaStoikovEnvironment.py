0a4ac4680b5e48b70de42fe26d1401c99e132794
JJJerome <JosephJerome94@gmail.com>
2022-03-01 11:48:23 +0000

    Refactor AS env for more flexibility

--- RL4MM/gym/AvellanedaStoikovEnvironment.py
+++ RL4MM/gym/AvellanedaStoikovEnvironment.py
@@ -2,8 +2,8 @@ import gym
 import numpy as np
 
 from copy import deepcopy
-from gym.spaces import Box, Discrete, Tuple
-from math import sqrt
+from gym.spaces import Box
+from math import sqrt, isclose
 
 from RL4MM.rewards.RewardFunctions import RewardFunction, PnL
 
@@ -26,6 +26,14 @@ class AvellanedaStoikovEnvironment(gym.Env):
         terminal_time: float = 1.0,
         n_steps: int = 200,
         reward_function: RewardFunction = None,
+        drift: float = 0.0,
+        volatility: float = 2.0,
+        arrival_rate: float = 140.0,
+        fill_exponent: float = 1.5,
+        max_inventory: int = 100,
+        initial_cash: float = 100.0,
+        initial_inventory: int = 0,
+        initial_stock_price: float = 100.0,
         continuous_observation_space: bool = True,  # This permits us to use out of the box algos from Stable-baselines
         seed: int = None,
     ):
@@ -33,6 +41,14 @@ class AvellanedaStoikovEnvironment(gym.Env):
         self.terminal_time = terminal_time
         self.n_steps = n_steps
         self.reward_function = reward_function or PnL()
+        self.drift = drift
+        self.volatility = volatility
+        self.arrival_rate = arrival_rate
+        self.fill_exponent = fill_exponent
+        self.max_inventory = max_inventory
+        self.initial_cash = initial_cash
+        self.initial_inventory = initial_inventory
+        self.initial_stock_price = initial_stock_price
         self.continuous_observation_space = continuous_observation_space
         self.rng = np.random.default_rng(seed)
 
@@ -40,41 +56,31 @@ class AvellanedaStoikovEnvironment(gym.Env):
         # observation space is (stock price, cash, inventory, step_number)
         self.observation_space = Box(
             low=np.zeros(4),
-            high=np.array([np.inf, np.inf, MAX_INVENTORY, n_steps]),
+            high=np.array([np.inf, np.inf, self.max_inventory, terminal_time]),
             dtype=np.float64,
         )
-
-        if not continuous_observation_space:
-            self.observation_space = Tuple(
-                (
-                    Box(low=0.0, high=np.inf, shape=(1, 1)),
-                    Box(low=0.0, high=np.inf, shape=(1, 1)),
-                    Discrete(MAX_INVENTORY),
-                    Discrete(n_steps),
-                )
-            )
         self.state: np.ndarray = np.array([])
         self.dt = self.terminal_time / self.n_steps
 
     def reset(self):
         self.state = np.array([INITIAL_STOCK_PRICE, INITIAL_CASH, INITIAL_INVENTORY, 0])
-        return self._convert_internal_state_to_obs(self.state)
+        return self.state
 
     def step(self, action: np.ndarray):
         next_state = self._get_next_state(action)
-        reward = self.reward_function.calculate(self.state, action, next_state)
+        done = isclose(next_state[3], self.terminal_time)  # due to floating point arithmetic
+        reward = self.reward_function.calculate(self.state, action, next_state, done)
         self.state = next_state
-        done = self.state[3] == self.n_steps
-        return self._convert_internal_state_to_obs(self.state), reward, done, {}
+        return self.state, reward, done, {}
 
     def render(self, mode="human"):
         pass
 
     def _get_next_state(self, action: np.ndarray) -> np.ndarray:
         next_state = deepcopy(self.state)
-        next_state[0] += DRIFT * self.dt + VOLATILITY * sqrt(self.dt) * self.rng.normal()
-        next_state[3] += 1
-        fill_prob_bid, fill_prob_ask = RATE_OF_ARRIVAL * np.exp(-FILL_EXPONENT * action) * self.dt
+        next_state[0] += self.drift * self.dt + self.volatility * sqrt(self.dt) * self.rng.normal()
+        next_state[3] += self.dt
+        fill_prob_bid, fill_prob_ask = self.arrival_rate * np.exp(-self.fill_exponent * action) * self.dt
         unif_bid, unif_ask = self.rng.random(2)
         if unif_bid > fill_prob_bid and unif_ask > fill_prob_ask:  # neither the agent's bid nor their ask is filled
             pass
@@ -88,14 +94,3 @@ class AvellanedaStoikovEnvironment(gym.Env):
         if unif_bid < fill_prob_bid and unif_ask < fill_prob_ask:  # both bid and ask filled
             next_state[1] += action[0] + action[1]
         return next_state
-
-    def _convert_internal_state_to_obs(self, state: np.ndarray):
-        if self.continuous_observation_space:
-            return state
-        else:
-            return (
-                np.array([[state[0]]], dtype=np.float32),
-                np.array([[state[1]]], dtype=np.float32),
-                state[2],
-                state[3],
-            )
