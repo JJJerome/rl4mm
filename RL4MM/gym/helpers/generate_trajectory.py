76f812012f49429a0635a6bff46eb63b55d00ab1
JJJerome <JosephJerome94@gmail.com>
2022-03-02 18:10:57 +0000

    Fix generate_trajectory for new "Action" type

--- RL4MM/gym/helpers/generate_trajectory.py
+++ RL4MM/gym/helpers/generate_trajectory.py
@@ -5,21 +5,19 @@ from RL4MM.agents.Agent import Agent
 
 
 def generate_trajectory(env: gym.Env, agent: Agent, seed: int = None):
-    action_dimension = env.action_space.shape[0]
-    observation_dimension = env.observation_space.shape[0]
     if seed is not None:
         np.random.seed(seed)
-    obs = env.reset()
-    observations = obs.reshape(1, observation_dimension)
-    action: np.ndarray = agent.get_action(obs)
-    actions = action.reshape(1, action_dimension)
+    observations = []
+    actions = []
     rewards = []
+    obs = env.reset()
+    observations.append(obs)
     while True:
-        obs, reward, done, _ = env.step(action)
-        observations = np.append(arr=observations, values=obs.reshape(1, observation_dimension), axis=0)
-        rewards.append(reward)
         action = agent.get_action(obs)
-        actions = np.append(arr=actions, values=action.reshape(1, action_dimension), axis=0)
+        obs, reward, done, _ = env.step(action)
+        actions.append(action)
+        observations.append(obs)
+        rewards.append(reward)
         if done:
             break
-    return observations, rewards, actions
+    return observations, actions, rewards
