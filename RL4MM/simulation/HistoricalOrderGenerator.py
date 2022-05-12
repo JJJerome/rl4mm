af3651476dd171d37d49e0ae469283113ec39320
Joe <JosephJerome94@gmail.com>
2022-05-12 10:07:32 +0100

    Add HistoricalOrderGenerator.py

--- RL4MM/simulation/HistoricalOrderGenerator.py
+++ RL4MM/simulation/HistoricalOrderGenerator.py
@@ -1,4 +1,6 @@
+from collections import deque
 from datetime import datetime
+from typing import Deque
 
 import pandas as pd
 
@@ -8,15 +10,16 @@ from RL4MM.simulation.OrderGenerator import OrderGenerator
 
 
 class HistoricalOrderGenerator(OrderGenerator):
-    def __init__(self, ticker: str = "MSFT", database: HistoricalDatabase = None ):
+    name = "historical"
+    def __init__(self, ticker: str = "MSFT", database: HistoricalDatabase = None):
         self.ticker = ticker
         self.database = database or HistoricalDatabase()
         self.exchange_name = "NASDAQ"  # Here, we are only using LOBSTER data for now
 
-    def generate_messages(self, start_date: datetime, end_date: datetime):
+    def generate_orders(self, start_date: datetime, end_date: datetime) -> Deque[Order]:
         messages = self.database.get_messages(start_date, end_date, self.exchange_name, self.ticker)
         messages = self._remove_hidden_executions(messages)
-
+        return deque(self._get_order_from_external_message(m) for m in messages)
 
     @staticmethod
     def _remove_hidden_executions(messages: pd.DataFrame):
@@ -33,4 +36,3 @@ class HistoricalOrderGenerator(OrderGenerator):
             ticker=message.ticker,
             external_id=message.external_id,
         )
-
