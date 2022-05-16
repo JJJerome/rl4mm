c2e77f59e1d43c85acca45bf7fc6269176636bdc
Joe <JosephJerome94@gmail.com>
2022-05-16 16:16:33 +0100

    Improve visuals on visualise_orderbook

--- RL4MM/orderbook/visualise_orderbook.py
+++ RL4MM/orderbook/visualise_orderbook.py
@@ -4,19 +4,28 @@ import plotly.express as px
 from RL4MM.orderbook.models import Orderbook
 
 
-def visualise_orderbook(orderbook: Orderbook, n_levels: int = 10):
+def visualise_orderbook(orderbook: Orderbook, n_levels: int = 10, tick_size: float = 0.01):
     df = convert_orderbook_to_dataframe(orderbook, n_levels)
-    fig = px.bar(df, x="price", y="volume", color="direction")  # , title=f"Orderbook at {x.name}")
+    fig = px.bar(
+        df,
+        x="price",
+        y="volume",
+        color="direction",
+        title=orderbook["ticker"],
+        color_discrete_sequence=["green", "red"],
+    )
+    fig.update_traces(width=tick_size)
     fig.show()
 
 
 def convert_orderbook_to_dataframe(orderbook: Orderbook, n_levels: int = 10):
     order_dict = {}
     for side in ["bid", "ask"]:
-        prices = reversed(orderbook[side]) if side == "bid" else orderbook[side]
+        prices = reversed(orderbook[side]) if side == "bid" else orderbook[side]  # type: ignore
         for level, price in enumerate(prices):
             if level >= n_levels:
                 break
-            order_dict[side + str(level)] = (side, price, sum(order.volume for order in orderbook[side][price]))
+            total_volume = sum(order.volume for order in orderbook[side][price])  # type: ignore
+            order_dict[side + str(level)] = (side, price, total_volume)
     df = pd.DataFrame(order_dict).T
     return df.rename(columns={0: "direction", 1: "price", 2: "volume"})
