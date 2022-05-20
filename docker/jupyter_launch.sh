b5863a9247c9140cdd846622c89092737a03edfe
Gregory Palmer <gpalmer@container.l3s.uni-hannover.de>
2022-05-21 00:32:14 +0200

    Added docker file and script for launching the container.

---
+++ docker/jupyter_launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+jupyter notebook --no-browser --port $1 --ip=0.0.0.0 --allow-root
+
---
+++ docker/start_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run --rm --gpus device=0 -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
