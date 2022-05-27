fb50acaed9ff16f38af4f6fd86ac9e936b5ca587
gjp1203 <gjp1203@gmail.com>
2022-05-27 12:16:49 +0200

    updated dockerfile

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --rm --gpus device=0 -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
+#docker run --rm --gpus device=0 -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
+#docker run --rm -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
+docker run --rm -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest /bin/bash #./jupyter_launch.sh $1
