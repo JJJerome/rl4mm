686f1881310fee5323ff5180fed38a0ffce9f089
Joe <JosephJerome94@gmail.com>
2022-05-27 09:08:30 +0100

    Add orderbook_comparison for debugging purposes

---
+++ RL4MM/extras/orderbook_comparison.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from RL4MM.orderbook.models import Orderbook
+
+
+def convert_to_lobster_format(orderbook: Orderbook, n_levels: int = 10):
+    lobster_book = dict()
+    for direction in ["buy", "sell"]:
+        half_book = orderbook[direction]
+        if direction == "buy":
+            half_book = reversed(half_book)
+        for level, price in enumerate(half_book):
+            if level < n_levels:
+                lobster_book[direction + "_price_" + str(level)] = price
+                volume = 0
+                for order in orderbook[direction][price]:
+                    volume += order.volume
+                lobster_book[direction + "_volume_" + str(level)] = volume
+    return lobster_book
+
+
+def compare_elements_of_books(book_1: pd.DataFrame, book_2: pd.DataFrame, verbose: bool = False):
+    if verbose:
+        for key in book_1.keys():
+            print(f"{key}: {book_1[key]==book_2[key]}")
+    else:
+        print(all([book_1[key] == book_2[key] for key in book_1.keys()]))
