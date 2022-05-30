fccdb333c5532870c512199116a7cb23e9b06c66
gjp1203 <gjp1203@gmail.com>
2022-05-30 11:59:18 +0200

    Changed base to ray, and added lines for building and initiating PSQL

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#docker run --rm --gpus device=0 -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
-#docker run --rm -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest ./jupyter_launch.sh $1
-docker run --rm -v ${PWD}/../:/workdir/RL4MM/ -p $1:$1 --name rl4mm  --user root -it rl4mm:latest /bin/bash #./jupyter_launch.sh $1
+docker run --rm -v ${PWD}/../:/home/RL4MM/ -p $1:$1 --name rl4mm --user root -dit rl4mm:latest ./launcher.sh $1
