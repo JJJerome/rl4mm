0cde848e6b3d7fbc5914f1e9d88c0f758c46555f
Gregory Palmer <gpalmer@container.l3s.uni-hannover.de>
2022-05-31 11:16:24 +0200

    Added GPU arg

---
+++ test_scripts/ppo_example.py
@@ -0,0 +1,28 @@
+from ray.rllib.agents.ppo import PPOTrainer
+
+# Configure the algorithm.
+iterations = 10
+config = {
+    "env": "MountainCarContinuous-v0",
+    "num_gpus": 1,
+    "num_workers": 2,
+    "framework": "torch",
+    "model": {
+        "fcnet_hiddens": [64, 64],
+        "fcnet_activation": "relu",
+        "use_lstm":True,
+    },
+    "evaluation_num_workers": 1,
+    "evaluation_config": {
+        "render_env": False,
+    },
+}
+
+trainer = PPOTrainer(config=config)
+print("Starting training")
+for i in range(iterations):
+    print(f"Iteration %d" % i)
+    print(trainer.train())
+print("Evaluating")
+print(trainer.evaluate())
+print("Done")
