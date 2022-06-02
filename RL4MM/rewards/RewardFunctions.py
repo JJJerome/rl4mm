8f66889dec53cf134dd30c5a7b37064eaf1209a2
Joe <JosephJerome94@gmail.com>
2022-06-02 17:25:48 +0100

    Add InventoryAdjustedPnL reward function

--- RL4MM/rewards/RewardFunctions.py
+++ RL4MM/rewards/RewardFunctions.py
@@ -14,3 +14,12 @@ class PnL(RewardFunction):
         current_value = current_state["cash"] + current_state["inventory"] * current_state["asset_price"]
         next_value = next_state["cash"] + next_state["inventory"] * next_state["asset_price"]
         return next_value - current_value
+
+
+class InventoryAdjustedPnL(RewardFunction):
+    def __init__(self, inventory_aversion: float):
+        self.inventory_aversion = inventory_aversion
+        self.pnl = PnL()
+
+    def calculate(self, current_state: InternalState, next_state: InternalState) -> float:
+        return self.pnl.calculate(current_state, next_state) - self.inventory_aversion * (next_state["inventory"]) ** 2
