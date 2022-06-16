8caed367582b604e01bff2c319a987b768f05055
JJJerome <JosephJerome94@gmail.com>
2022-06-16 18:09:17 +0100

    Update run_populate_database to take multiple comma separated trading_dates

--- run_populate_database.py
+++ run_populate_database.py
@@ -4,7 +4,7 @@ from RL4MM.database.populate_database import populate_database
 
 parser = argparse.ArgumentParser(description="Populate a postgres database with LOBSTER data")
 parser.add_argument("--ticker", action="store", type=str, default="MSFT", help="the ticker to add")
-parser.add_argument("--trading_date", action="store", type=str, default="2012-06-21", help="the date to add")
+parser.add_argument("--trading_dates", action="store", type=str, default="2012-06-21", help="the dates to add")
 parser.add_argument("--n_levels", action="store", type=int, default=200, help="the number of orderbook levels")
 parser.add_argument(
     "--path_to_lobster_data",
@@ -37,10 +37,9 @@ parser.add_argument(
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
     populate_database(
         tickers=(args.ticker,),
-        trading_dates=(args.trading_date,),
+        trading_dates=(args.trading_dates.split(","),),
         n_levels=args.n_levels,
         path_to_lobster_data=args.path_to_lobster_data,
         book_snapshot_freq=args.book_snapshot_freq,
