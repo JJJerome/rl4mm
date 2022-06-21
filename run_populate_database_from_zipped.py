72dbda3054a7541696ae0b551a33f027b52597fc
Joe <JosephJerome94@gmail.com>
2022-06-21 12:47:53 +0100

    Add first version of run_populate_database_from_zipped.py

--- run_populate_database_from_zipped.py
+++ run_populate_database_from_zipped.py
@@ -1,12 +1,18 @@
 import argparse
+import logging
+from subprocess import run
 
-import pandas as pd
+from RL4MM.utils.utils import (
+    get_date_time,
+    get_next_trading_dt,
+    get_last_trading_dt,
+    get_trading_datetimes,
+    daterange_in_db,
+)
 
 from RL4MM.database.populate_database import populate_database
-from RL4MM.utils.utils import get_date_time
 
 parser = argparse.ArgumentParser(description="Populate a postgres database with LOBSTER data")
-parser.add_argument("--ticker", action="store", type=str, default="MSFT", help="the ticker to add")
 parser.add_argument("-nl", "--n_levels", action="store", type=int, default=200, help="the number of orderbook levels")
 parser.add_argument(
     "--path_to_lobster_data",
@@ -24,14 +30,6 @@ parser.add_argument(
     help="the frequency of book snapshots added to database",
 )
 parser.add_argument(
-    "-mr",
-    "--max_rows",
-    action="store",
-    type=int,
-    default=99999999999,
-    help="the max number of rows to add for a given date",
-)
-parser.add_argument(
     "-bs",
     "--batch_size",
     action="store",
@@ -42,13 +40,24 @@ parser.add_argument(
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
-    populate_database(
-        tickers=(args.ticker,),
-        trading_datetimes=pd.bdate_range(get_date_time(args.min_trading_date), get_date_time(args.max_trading_date)),
-        n_levels=args.n_levels,
-        path_to_lobster_data=args.path_to_lobster_data,
-        book_snapshot_freq=args.book_snapshot_freq,
-        max_rows=args.max_rows,
-        batch_size=args.batch_size,
-    )
+    filenames = run(["ls", args.path_to_lobster_data], capture_output=True).stdout.decode("utf-8").split("\n")
+    for filename in filenames[:1]:
+        if len(filename) == 0:
+            continue
+        ticker = filename.split("_")[-4]
+        start_date = get_date_time(filename.split("_")[-3])
+        end_date = get_date_time(filename.split("_")[-2])
+        n_levels = int(filename.split("_")[-1].split(".")[0])
+        next_trading_dt = get_next_trading_dt(start_date)
+        last_trading_dt = get_last_trading_dt(end_date)
+        if daterange_in_db(next_trading_dt, last_trading_dt):
+            logging.info(f"Data for {ticker} between {start_date} and {end_date} already in database so not re-added.")
+            continue
+        run(["7z", "x", filename])
+        populate_database(
+            tickers=(ticker,),
+            trading_datetimes=get_trading_datetimes(start_date, end_date),
+            path_to_lobster_data=args.path_to_lobster_data,
+            book_snapshot_freq=args.book_snapshot_freq,
+            batch_size=args.batch_size,
+        )
