65f4bd215387c74b2a21e55b7581cb2465f6aec3
Rahul Savani <rahul.savani@gmail.com>
2022-06-23 19:34:34 +0100

    tag variable in start_container.sh

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --rm --shm-size=10.24gb --gpus device=0 -v $2:/home/data/ -v ${PWD}/../:/home/RL4MM/ -p $1:$1 -p 6006:6006 --name rl4mm --user root -dit rl4mm:populated_db ./launcher.sh $1
+#tag='latest'
+tag='populated_db'
+docker run --rm --shm-size=10.24gb --gpus device=0 -v $2:/home/data/ -v ${PWD}/../:/home/RL4MM/ -p $1:$1 -p 6006:6006 --name rl4mm --user root -dit rl4mm:$tag ./launcher.sh $1
