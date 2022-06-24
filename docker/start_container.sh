b7adc1fd45bca98be294e26c679faecaff7b7415
Rahul Savani <rahul.savani@gmail.com>
2022-06-24 09:13:21 +0100

    docker README.md

--- docker/start_container.sh
+++ docker/start_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --shm-size=10.24gb --gpus device=0 -v $2:/home/data/ -v ${PWD}/../:/home/RL4MM/ -p $1:$1 -p 6006:6006 --name rl4mm --user root -dit rl4mm:populated_db ./launcher.sh $1
+docker run --rm --shm-size=10.24gb --gpus device=0 -v $2:/home/data/ -v ${PWD}/../:/home/RL4MM/ -p $1:$1 -p 6006:6006 --name rl4mm --user root -dit rl4mm:latest ./launcher.sh $1
