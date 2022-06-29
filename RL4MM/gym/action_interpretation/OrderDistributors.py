26ae3de0adb31a38fd1179087538b19cfe7923e2
Joe <JosephJerome94@gmail.com>
2022-06-29 12:30:03 +0100

    Re-add eps to BetaOrderDistributor

--- RL4MM/gym/action_interpretation/OrderDistributors.py
+++ RL4MM/gym/action_interpretation/OrderDistributors.py
@@ -29,12 +29,12 @@ class BetaOrderDistributor(OrderDistributor):
         self.active_volume = active_volume
         self.c = concentration
 
-    def _convert_action(self, action: np.ndarray) -> dict[Literal["buy", "sell"], tuple[np.ndarray]]:
+    def _convert_action(self, action: np.ndarray, eps=1e-5) -> dict[Literal["buy", "sell"], tuple[np.ndarray]]:
         assert all(action) > 0, "Action must be positive"
         (a_buy, b_buy) = (action[0], self.c - action[0]) if self.c is not None else (action[0], action[1])
         (a_sell, b_sell) = (action[1], self.c - action[1]) if self.c is not None else (action[2], action[3])
-        beta_binom_buy = betabinom(n=self.n_levels - 1, a=a_buy, b=b_buy)
-        beta_binom_sell = betabinom(n=self.n_levels - 1, a=a_sell, b=b_sell)
+        beta_binom_buy = betabinom(n=self.n_levels - 1, a=a_buy, b=max(b_buy, eps))
+        beta_binom_sell = betabinom(n=self.n_levels - 1, a=a_sell, b=max(b_sell, eps))
         buy_volumes = np.round(beta_binom_buy.pmf(self.tick_range) * self.active_volume).astype(int)
         sell_volumes = np.round(beta_binom_sell.pmf(self.tick_range) * self.active_volume).astype(int)
         return {"buy": buy_volumes, "sell": sell_volumes}
