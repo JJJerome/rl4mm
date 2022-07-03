653d9df3c03184d66b322aa7c17284db51cbb250
Rahul Savani <rahul.savani@gmail.com>
2022-07-03 11:46:10 +0100

    experiments/fixed_action_sweep.py

--- experiments/fixed_action_sweep.py
+++ experiments/fixed_action_sweep.py
@@ -8,9 +8,22 @@ agents = list()
 # THIS WAS THE OLD SWEEP
 ##############################################################################
 
+# for default_alpha in [1,2,5,10]:
+    # for default_beta in [1,2,5,10]:
+        # for max_inventory in [100,500,1000]:
+            # agents.append(FixedActionAgent(np.array([default_alpha,
+                                                     # default_beta,
+                                                     # default_alpha,
+                                                     # default_beta, 
+                                                     # max_inventory])))
+
+##############################################################################
+# NEW TEST SWEEP
+##############################################################################
+
 for default_alpha in [1,2,5,10]:
     for default_beta in [1,2,5,10]:
-        for max_inventory in [100,500,1000]:
+        for max_inventory in [1000,5000]:
             agents.append(FixedActionAgent(np.array([default_alpha,
                                                      default_beta,
                                                      default_alpha,
