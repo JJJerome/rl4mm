417bd28649c0bccd0fa829a4c307629217957855
Rahul Savani <rahul.savani@gmail.com>
2022-07-03 22:32:46 +0100

    process_sweep_results.py

---
+++ process_sweep_results.py
@@ -0,0 +1,48 @@
+import os
+import glob
+import json
+import pandas as pd
+
+def read_json(fpath):
+    with open(fpath) as json_file:
+        data = json.load(json_file)
+    return data
+
+def fname_to_dict(fname):
+    tmp = fname.split("_")
+    d = dict()
+
+    d['strategy']   = tmp[0]
+    d['alpha_bid']  = tmp[1]
+    d['beta_bid']   = tmp[2]
+    d['alpha_ask']  = tmp[3]
+    d['beta_ask']   = tmp[4]
+    d['ticker']     = tmp[5]
+    d['minq']       = tmp[11]
+
+    return d
+
+if __name__ == '__main__':
+
+    fpath= '/Users/rahul/Dropbox/titan_sweep_results/outputs/jsons'
+
+    lst = []
+
+    for fpath in glob.glob(fpath + '/*.json'):
+
+        data = read_json(fpath)
+
+        # print(fpath)
+        # print(data)
+
+        fname = os.path.basename(fpath) 
+
+        rewards_df = pd.DataFrame(data['rewards'])
+
+        tmp = fname_to_dict(fname)
+        tmp['mean'] = float(rewards_df.describe().loc['mean'])
+
+        lst.append(tmp)
+
+    df = pd.DataFrame.from_records(lst)
+    print(df.sort_values('minq'))
