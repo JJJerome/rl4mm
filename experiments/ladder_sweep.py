a2c221e7d704268a926e2b7762c3e87a038f9b62
Joe <JosephJerome94@gmail.com>
2022-07-03 17:13:55 +0100

    BUGFIX: env config deepcopy needs to be inside for loop

--- experiments/ladder_sweep.py
+++ experiments/ladder_sweep.py
@@ -10,8 +10,8 @@ N_LEVELS = 10
 def get_env_configs_and_agents(env_config:dict):
     env_configs = list()
     agents = [FixedActionAgent(np.array([1, 1, 1, 1]))]
-    env_conf = deepcopy(env_config)
     for inner in INNER_QUOTES:
+        env_conf = deepcopy(env_config)
         env_conf["min_quote_level"] = inner
         env_conf["max_quote_level"] = inner + N_LEVELS
         env_configs.append(env_conf)
