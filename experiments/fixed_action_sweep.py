d7a8090d4de117f2bb870f9dc03f9689790cf434
Rahul Savani <rahul.savani@gmail.com>
2022-07-03 10:08:55 +0100

    experiments/fixed_action_sweep.py

---
+++ experiments/fixed_action_sweep.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from RL4MM.agents.baseline_agents import FixedActionAgent, ContinuousTeradactyl
+
+agents = list()
+
+##############################################################################
+# THIS WAS THE OLD SWEEP
+##############################################################################
+
+for default_alpha in [1,2,5,10]:
+    for default_beta in [1,2,5,10]:
+        for max_inventory in [100,500,1000]:
+            agents.append(FixedActionAgent(np.array([default_alpha,
+                                                     default_beta,
+                                                     default_alpha,
+                                                     default_beta, 
+                                                     max_inventory])))
