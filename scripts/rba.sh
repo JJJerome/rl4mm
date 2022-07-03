67abaafd4daed9e82b7ffad60ea03507e1c9a117
Rahul Savani <rahul.savani@gmail.com>
2022-07-03 11:50:06 +0100

    scripts/rba.sh reads ticker file from scripts/$TICKER so assumes it is run from root of repo

--- scripts/rba.sh
+++ scripts/rba.sh
@@ -7,4 +7,4 @@ maxd="2022-03-13" # ?
 while read -r ticker; do 
 	echo "Processing $ticker";
 	python rule_based_main.py -mind $mind -maxd $maxd --ticker $ticker;
-done < $TICKERS_FILE
+done < scripts/$TICKERS_FILE # assumes it is run from root of repo
