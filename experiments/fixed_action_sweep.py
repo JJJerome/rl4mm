8df81f2ffb720b26fb0c55c41bb2cafe7be8adeb
Joe <JosephJerome94@gmail.com>
2022-07-04 10:02:42 +0100

    Remove ladder strat from fixed action sweep

--- experiments/fixed_action_sweep.py
+++ experiments/fixed_action_sweep.py
@@ -6,8 +6,8 @@ from RL4MM.agents.baseline_agents import FixedActionAgent
 # NEW TEST SWEEP
 ##############################################################################
 
-DEFAULT_ALPHAS = [1, 2, 5, 10]
-DEFAULT_BETAS = [1, 2, 5, 10]
+DEFAULT_ALPHAS = [1, 2, 5]
+DEFAULT_BETAS = [1, 2, 5]
 
 
 def get_env_configs_and_agents(env_config:dict):
