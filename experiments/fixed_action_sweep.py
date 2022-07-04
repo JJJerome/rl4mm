e29770679856d735e46d537609a315cda24d1b9c
Joe <JosephJerome94@gmail.com>
2022-07-04 10:00:31 +0100

    Skip ladder strategy in fixed action sweep

--- experiments/fixed_action_sweep.py
+++ experiments/fixed_action_sweep.py
@@ -14,6 +14,9 @@ def get_env_configs_and_agents(env_config:dict):
     agents = list()
     for default_alpha in DEFAULT_ALPHAS:
         for default_beta in DEFAULT_BETAS:
+            if default_alpha == 1 and default_beta == 1:
+                print("Skipping alpha=beta=1 parameter combination as it is a ladder strategy")
+                continue
             agents.append(FixedActionAgent(np.array([default_alpha,
                                                      default_beta,
                                                      default_alpha,
