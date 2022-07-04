870a00582c65d2fb6dd4676ada463f4cc20a9b57
Joe <JosephJerome94@gmail.com>
2022-07-04 17:33:42 +0100

    Supress beta zero division warning

--- RL4MM/gym/action_interpretation/OrderDistributors.py
+++ RL4MM/gym/action_interpretation/OrderDistributors.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import abc
 import sys
+import warnings
+from contextlib import suppress
 
 if sys.version_info[0] == 3 and sys.version_info[1] >= 8:
     from typing import Literal
@@ -10,10 +12,12 @@ else:
 import numpy as np
 from scipy.stats import beta
 
+EPS = 0.000001
+
 
 class OrderDistributor(metaclass=abc.ABCMeta):
     def convert_action(self, action: np.ndarray) -> dict[Literal["buy", "sell"], tuple[np.ndarray]]:
-        action = np.array(action, dtype=float) + sys.float_info.min
+        action = np.array(action, dtype=float) + EPS  # for strict positivity
         return self._convert_action(action)
 
     @abc.abstractmethod
@@ -39,8 +43,10 @@ class BetaOrderDistributor(OrderDistributor):
         (a_sell, b_sell) = (action[1], self.c - action[1]) if self.c is not None else (action[2], action[3])
         beta_buy = self.distribution(a=a_buy, b=b_buy)
         beta_sell = self.distribution(a=a_sell, b=b_sell)
-        buy_dist = np.array([beta_buy.pdf(midpoint) for midpoint in self.midpoints])
-        sell_dist = np.array([beta_sell.pdf(midpoint) for midpoint in self.midpoints])
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            buy_dist = np.array([beta_buy.pdf(midpoint) for midpoint in self.midpoints])
+            sell_dist = np.array([beta_sell.pdf(midpoint) for midpoint in self.midpoints])
         buy_dist /= buy_dist.sum()  # normalise to one
         sell_dist /= sell_dist.sum()  # normalise to one
         buy_volumes = np.round(buy_dist * self.active_volume).astype(int)
