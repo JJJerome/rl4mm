6b9f45e83c4adecaa057205daecdcc3c7ecefca7
Joe <JosephJerome94@gmail.com>
2022-07-05 12:58:19 +0100

    Add kappa scaling and max_inv sweep

--- experiments/teradactyl_sweep.py
+++ experiments/teradactyl_sweep.py
@@ -1,13 +1,13 @@
 min_quote_range = [0]  # [1, 0, -1]
 max_quote_range = [10]  # [10, 15, 20]
-max_inv_range = [500]  # [1000]
-default_omega_range = [0.1,0.2,0.3]
-default_kappa_range = [5, 10]
+default_omega_range = [0.2]  # to reduce. Choose one
+default_kappa_range = [5]  # maybe to reduce. Choose one
 
 from RL4MM.agents.baseline_agents import ContinuousTeradactyl
 
-max_inv = 500
-max_kappa = 20
+
+kappa_scaling_range = [3,5,10]
+max_inv_range = [200,500,1000]
 
 
 def get_env_configs_and_agents(env_config:dict):
@@ -16,10 +16,12 @@ def get_env_configs_and_agents(env_config:dict):
 
     for default_omega in default_omega_range:
         for default_kappa in default_kappa_range:
-            agents.append(ContinuousTeradactyl(max_inventory=max_inv,
-                                               default_kappa=default_kappa,
-                                               default_omega=default_omega,
-                                               max_kappa=default_kappa*3))
+            for kappa_scaling in kappa_scaling_range:
+                for max_inv in max_inv_range:
+                    agents.append(ContinuousTeradactyl(max_inventory=max_inv,
+                                                       default_kappa=default_kappa,
+                                                       default_omega=default_omega,
+                                                       max_kappa=default_kappa*kappa_scaling))
 
     return env_configs, agents
 
