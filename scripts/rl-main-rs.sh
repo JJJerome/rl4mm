2c17995cce1fc70c04687d0652a93628da438acf
Rahul Savani <rahul.savani@gmail.com>
2022-07-22 18:37:22 +0100

    2018 -> 2022 in rl- scripts

--- scripts/rl-main-rs.sh
+++ scripts/rl-main-rs.sh
@@ -5,7 +5,7 @@ echo "Using tickers file: $TICKERS_FILE"
 while read -r ticker; do
 	echo "Processing $ticker";
 	python python main.py -mind 2022-03-01\
-               -maxd 2018-03-10\
+               -maxd 2022-03-10\
                -minde 2022-03-11\
                -maxde 2022-03-11\
                --ticker $ticker\
