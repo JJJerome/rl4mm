b114a9744a2779238eee11a89aba5d6f16f4143a
joe <JosephJerome94@gmail.com>
2022-07-22 18:32:10 +0100

    Add rolling sharpe script

---
+++ scripts/rl-main-rs.sh
@@ -0,0 +1,16 @@
+TICKERS_FILE=$1
+
+echo "Using tickers file: $TICKERS_FILE"
+
+while read -r ticker; do
+	echo "Processing $ticker";
+	python python main.py -mind 2022-03-01\
+               -maxd 2018-03-10\
+               -minde 2022-03-11\
+               -maxde 2022-03-11\
+               --ticker $ticker\
+               -el 5\
+               -psr RS\
+               -epsr RS;
+
+done < scripts/$TICKERS_FILE # assumes it is run from root of repo
