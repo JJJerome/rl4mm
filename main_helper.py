512dce2c4591ecb412e84cc35d5a7155ad280fa7
joe <JosephJerome94@gmail.com>
2022-08-10 13:11:52 +0100

    Reformat

--- main_helper.py
+++ main_helper.py
@@ -282,9 +282,11 @@ def get_ray_config(args, env_config, eval_env_config, name, cmc=None):
             "output_max_file_size": args["output_max_file_size"],
             # --------------- Tuning: ---------------------
             "num_sgd_iter": tune.choice([10, 20, 30, 100, 500]),
-            "sgd_minibatch_size": tune.choice([2**3,2**5,2**7, 2**9, 2**11]),
+            "sgd_minibatch_size": tune.choice([2**3, 2**5, 2**7, 2**9, 2**11]),
             "train_batch_size": tune.choice([2**11, 2**12, 2**13, 2**14, 2**15]),
-            "rollout_fragment_length": tune.choice([2**8, 2**9, 2**10, 2**12, 2**13, 2**14]),  # args["rollout_fragment_length"],
+            "rollout_fragment_length": tune.choice(
+                [2**8, 2**9, 2**10, 2**12, 2**13, 2**14]
+            ),  # args["rollout_fragment_length"],
             # "recreate_failed_workers": False, # Get an error for some reason when this is enabled.
             # "disable_env_checking": True,
             # 'seed':tune.choice(range(1000)),
