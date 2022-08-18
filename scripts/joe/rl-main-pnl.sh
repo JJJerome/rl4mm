5b44ae9a55d49e88e0c6301667bc85859d1f0146
joe <JosephJerome94@gmail.com>
2022-08-18 11:46:48 +0100

    Add model path

--- scripts/joe/rl-main-pnl.sh
+++ scripts/joe/rl-main-pnl.sh
@@ -4,12 +4,13 @@ echo "Using tickers file: $TICKERS_FILE"
 
 while read -r ticker; do
 	echo "Processing $ticker";
-	python main.py -en pnl-small-learning-rates\
+	python main.py -en pnl-small-learning-rates \
 	             -mind 2022-03-01\
                -maxd 2022-03-10 \
                -minde 2022-03-11 \
                -maxde 2022-03-11 \
                --ticker $ticker \
+               -mp /home/RL4MM/ray_results/tensorboard/JPM/PnL/concentration_None/full_state/normalisation_on_False/moc_False/PPO/PPO_HistoricalOrderbookEnvironment_e6821_00007_7_lr=0.0001_2022-08-17_21-05-41/checkpoint_000004 \
                -sgdi 20 \
                -sgdb 2048 \
                -rfl 1024 \
