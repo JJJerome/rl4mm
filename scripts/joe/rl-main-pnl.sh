cc73111f723190ac2f14fab1791a34fd05870f7e
joe <JosephJerome94@gmail.com>
2022-08-18 11:53:08 +0100

    Add model path

--- scripts/joe/rl-main-pnl.sh
+++ scripts/joe/rl-main-pnl.sh
@@ -10,7 +10,6 @@ while read -r ticker; do
 		             -minde 2022-03-11 \
 		             -maxde 2022-03-11 \
 		             --ticker $ticker \
-		             -mp "/home/RL4MM/ray_results/tensorboard/JPM/PnL/concentration_None/full_state/normalisation_on_False/moc_False/PPO/PPO_HistoricalOrderbookEnvironment_e6821_00007_7_lr=0.0001_2022-08-17_21-05-41/checkpoint_000004" \
 		             -sgdi 20 \
 		             -sgdb 2048 \
 		             -rfl 1024 \
@@ -20,6 +19,7 @@ while read -r ticker; do
 		             -psr PnL \
 		             -nepw 6 \
 		             -epsr PnL \
-		             -wb True; # -mp "/home/data/best_model/checkpoint_000100/checkpoint-100" \
+		             -wb True \
+		             -mp "/home/RL4MM/ray_results/tensorboard/JPM/PnL/concentration_None/full_state/normalisation_on_False/moc_False/PPO/PPO_HistoricalOrderbookEnvironment_e6821_00007_7_lr=0.0001_2022-08-17_21-05-41/checkpoint_000004"; # -mp "/home/data/best_model/checkpoint_000100/checkpoint-100" \
 
 done < scripts/$TICKERS_FILE # assumes it is run from root of repo
