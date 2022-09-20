090cca04d6b3f35268b0a8d0576e81b64907325f
JJJerome <JosephJerome94@gmail.com>
2022-09-20 11:48:03 +0100

    Remove dist and build

--- build/lib/rl4mm/agents/Agent.py
+++
@@ -1,8 +0,0 @@
-import abc
-import numpy as np
-
-
-class Agent(metaclass=abc.ABCMeta):
-    @abc.abstractmethod
-    def get_action(self, state: np.ndarray) -> np.ndarray:
-        pass
--- build/lib/rl4mm/helpers/env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import find_dotenv, load_dotenv
-
-
-def setup_environment_variables(dotenv_filename: str = ".env") -> None:
-    try:
-        dotenv_path = find_dotenv(filename=dotenv_filename, raise_error_if_not_found=True)
-    except IOError:
-        raise IOError(f"Could not find dotenv file: {dotenv_filename}")
-    load_dotenv(dotenv_path=dotenv_path, override=False)
